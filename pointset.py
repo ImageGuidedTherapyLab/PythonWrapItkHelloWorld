3034ff6068eab8297f12e25002bf0ba16fb1ba60
David Fuentes <fuentesdt@gmail.com>
2013-02-20 12:26:57 -0600

    cells and lines

--- pointset.py
+++ pointset.py
@@ -27,28 +27,36 @@ print pp
 
 # write vtk points file
 vtkpoints= vtk.vtkPoints()
-vertices= vtk.vtkCellArray()
-vertices.InsertNextCell( 1 ); vertices.InsertCellPoint( vtkpoints.InsertNextPoint(0,0,0) )
-vertices.InsertNextCell( 1 ); vertices.InsertCellPoint( vtkpoints.InsertNextPoint(2,3,4) )
-vertices.InsertNextCell( 1 ); vertices.InsertCellPoint( vtkpoints.InsertNextPoint(5,9,2) )
-vertices.InsertNextCell( 1 ); vertices.InsertCellPoint( vtkpoints.InsertNextPoint(4,5,6) )
-vertices.InsertNextCell( 1 ); vertices.InsertCellPoint( vtkpoints.InsertNextPoint(7,8,5) )
-vertices.InsertNextCell( 1 ); vertices.InsertCellPoint( vtkpoints.InsertNextPoint(0,0,1) )
-vertices.InsertNextCell( 1 ); vertices.InsertCellPoint( vtkpoints.InsertNextPoint(0,1,0) )
-vertices.InsertNextCell( 1 ); vertices.InsertCellPoint( vtkpoints.InsertNextPoint(1,0,0) )
-vertices.InsertNextCell( 1 ); vertices.InsertCellPoint( vtkpoints.InsertNextPoint(0,1,1) )
-vertices.InsertNextCell( 1 ); vertices.InsertCellPoint( vtkpoints.InsertNextPoint(1,1,0) )
+vtkpoints.InsertNextPoint(0,0,0) 
+vtkpoints.InsertNextPoint(2,3,4) 
+vtkpoints.InsertNextPoint(5,9,2) 
+vtkpoints.InsertNextPoint(4,5,6) 
+vtkpoints.InsertNextPoint(7,8,5) 
+vtkpoints.InsertNextPoint(0,0,1) 
+vtkpoints.InsertNextPoint(0,1,0) 
+vtkpoints.InsertNextPoint(1,0,0) 
+vtkpoints.InsertNextPoint(0,1,1) 
+vtkpoints.InsertNextPoint(1,1,0) 
+
+lines = vtk.vtkCellArray()
+
+for iii in range(9):
+  line = vtk.vtkLine()
+  line.GetPointIds().SetId(0,iii)
+  line.GetPointIds().SetId(1,iii+1)
+  lines.InsertNextCell( line );
 
 polydata = vtk.vtkPolyData()
 polydata.SetPoints(vtkpoints)
-polydata.SetVerts( vertices )
+polydata.SetLines( lines  )
 # data pointer
 pd = polydata.GetPointData()
+cd = polydata.GetCellData()
 DeepCopy = 1
 # color the points
-vtkdataarray = vtkNumPy.numpy_to_vtk( range(10) , DeepCopy) 
+vtkdataarray = vtkNumPy.numpy_to_vtk( range(9) , DeepCopy) 
 vtkdataarray.SetName("similarity")
-pd.AddArray( vtkdataarray )
+cd.AddArray( vtkdataarray )
 # displacments
 vtkvectorarray = vtkNumPy.numpy_to_vtk( [(1,1,2),(1,2,2),(1,9,2),(4,1,2),(7,1,2),(6,1,2),(1,4,2),(1,3,2),(1,1,0),(0,1,2)] , DeepCopy) 
 vtkvectorarray.SetName("displacement")
